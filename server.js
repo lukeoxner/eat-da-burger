f994d69f7e08e4b95ba9380e239aa3d113ec3b7b
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-03-01 17:47:01 -0700

    work on server.js

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
-// Setting up dependencies
+// setting up dependencies
 const express = require('express');
 const PORT = process.env.PORT || 8080;
 const app = express();
+const exphbs = require('express-handlebars');
+const routes = require('./controllers/burgersController.js');
+
+// setting up middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.use(routes);
+
+app.listen(PORT, () => {
+	console.log(`Server listening on Port ${PORT}`);
+});
