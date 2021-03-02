cf512713d4b603d6c7a4b1d6948fb9f6f0e58fec
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-03-01 21:49:00 -0700

    add jawsurl details

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@
 const mysql = require('mysql');
 
 // setting up connection
-const connection = mysql.createConnection({
-	host: 'localhost',
-	port: 3306,
-	user: 'root',
-	password: 'password',
-	database: 'burger_db',
-});
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	const connection = mysql.createConnection({
+		host: 'localhost',
+		port: 3306,
+		user: 'root',
+		password: 'password',
+		database: 'burger_db',
+	});
+}
 
 // making connection
 connection.connect((err) => {
