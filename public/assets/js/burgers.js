70d291e9a943240703f2f4832786ea014af81654
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-03-01 19:55:07 -0700

    work on handlebars

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,62 @@
+// ensure DOM loaded before attaching event handlers
+document.addEventListener('DOMContentLoaded', (event) => {
+	// UPDATE
+	const changeDevouredBtns = document.querySelectorAll('.change-devoured');
+
+	// Set up the event listener for the create button
+	if (changeDevouredBtns) {
+		changeDevouredBtns.forEach((button) => {
+			button.addEventListener('click', (e) => {
+				const id = e.target.getAttribute('data-id');
+				const newDevoured = e.target.getAttribute('data-newdevoured');
+
+				const newDevouredState = {
+					devoured: newDevoured,
+				};
+
+				fetch(`/api/burgers/${id}`, {
+					method: 'PUT',
+					headers: {
+						Accept: 'application/json',
+						'Content-Type': 'application/json',
+					},
+
+					body: JSON.stringify(newDevouredState),
+				}).then((response) => {
+					if (response.ok) {
+						location.reload('/');
+					} else {
+						alert('Houston we got a problem!');
+					}
+				});
+			});
+		});
+	}
+
+	// CREATE
+	const createBurgerBtn = document.getElementById('create-form');
+
+	if (createBurgerBtn) {
+		createBurgerBtn.addEventListener('submit', (e) => {
+			e.preventDefault();
+
+			const newBurger = {
+				name: document.getElementById('burger').value.trim(),
+				devoured: document.getElementById('devoured').checked,
+			};
+
+			fetch('/api/burgers', {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+
+				body: JSON.stringify(newBurger),
+			}).then(() => {
+				document.getElementById('burger').value = '';
+				location.reload();
+			});
+		});
+	}
+});
