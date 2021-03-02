06dcf60ee17460abdc53772e4b4c7b7697d8e2c5
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-03-01 18:26:09 -0700

    work on orm

--- config/orm.js
+++ config/orm.js
@@ -3,8 +3,21 @@ const connection = require('./connection.js');
 
 // setting up the object relational mapping (ORM)
 const orm = {
-	selectAll(table, cb) {},
-	insertOne() {},
+	selectAll(table, cb) {
+		const queryString = `SELECT * FROM ${table}`;
+		connection.query(queryString, (err, result) => {
+			if (err) throw err;
+			cb(result);
+		});
+	},
+	insertOne(table, vals, cb) {
+		const queryString = `INSERT INTO ${table} (burger_name, devoured) VALUES (?, ?)`;
+
+		connection.query(queryString, vals, (err, result) => {
+			if (err) throw err;
+			cb(result);
+		});
+	},
 	updateOne() {},
 };
 
--- models/burger.js
+++ models/burger.js
@@ -5,8 +5,8 @@ const burger = {
 	selectAll(cb) {
 		orm.all('burgers', (res) => cb(res));
 	},
-	insertOne(cols, vals, cb) {
-		orm.create('burgers', cols, vals, (res) => cb(res));
+	insertOne(vals, cb) {
+		orm.create('burgers', vals, (res) => cb(res));
 	},
 	updateOne(objColVals, condition, cb) {
 		orm.update('burgers', objColVals, condition, (res) => cb(res));
